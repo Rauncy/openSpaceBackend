1aac9101b65b89567693831048cecfe107dc753f
Chris <christopher.2927@gmail.com>
2019-11-09 19:41:07 -0600

    More formatting for web server

--- site/index.js
+++ site/index.js
@@ -2,7 +2,34 @@ const buildingSelect = document.getElementById("buildings");
 
 function updateBuilding(){
   console.log(buildingSelect.value);
+  loadData();
 }
 
 buildingSelect.addEventListener("change", updateBuilding, false);
 console.log(buildingSelect);
+
+var req = new XMLHttpRequest();
+req.onload = function(){
+  if(this.status == 200){
+		console.log(req.responseText);
+    var data = JSON.parse(req.responseText);
+
+    data = data[buildingSelect.value];
+
+    if(data){
+      Object.keys(data).forEach(v => {
+        ret+=`<tr><td>${v}</td><td>${data[v]}</td></tr>`;
+      });
+    }
+
+    document.getElementById("results").innerHTML = ret;
+  }
+};
+
+function loadData(){
+  if(buildingSelect.value!=""){
+    req.open("GET", "/requestData", true);
+    req.send();
+    setTimeout(loadData(), "60000");
+  }
+}
