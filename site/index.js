edc71123f495c1e0c8f59cb1846689518c5dfda1
Chris <christopher.2927@gmail.com>
2019-11-09 22:57:13 -0600

    Completed XHR for index page

--- site/index.js
+++ site/index.js
@@ -10,15 +10,17 @@ console.log(buildingSelect);
 
 var req = new XMLHttpRequest();
 req.onload = function(){
+  console.log("LOAD");
   if(this.status == 200){
 		console.log(req.responseText);
     var data = JSON.parse(req.responseText);
-
     data = data[buildingSelect.value];
+    let ret = "";
 
     if(data){
       Object.keys(data).forEach(v => {
-        ret+=`<tr><td>${v}</td><td>${data[v]}</td></tr>`;
+
+        ret+=`<tr><td>${v}</td><td>${data[v].data}</td></tr>`;
       });
     }
 
@@ -28,8 +30,9 @@ req.onload = function(){
 
 function loadData(){
   if(buildingSelect.value!=""){
-    req.open("GET", "/requestData", true);
+    console.log("REQ");
+    req.open("GET", "/_get/", true);
     req.send();
-    setTimeout(loadData(), "60000");
+    // setTimeout(loadData(), 60000);
   }
 }
